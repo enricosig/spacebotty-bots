212f7ac2bc8b9c7911c6f5b30346e9f72270414f
enrico.sighinolfi@outlook.com <enrico.sighinolfi@outlook.com>
2025-10-08 10:40:59 +0200

    chore: add /api/ping healthcheck

--- api/ping.py
+++ api/ping.py
@@ -3,6 +3,6 @@ from http.server import BaseHTTPRequestHandler
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
-        self.send_header("Content-Type", "text/plain")
+        self.send_header("Content-Type", "text/plain; charset=utf-8")
         self.end_headers()
         self.wfile.write(b"PONG")
